1713722e15ab8678a19e07fe876d0b25fb98aec5
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-10 22:36:08 +0200

    Refactor Brand model and migration

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,8 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Model
-{
+class Brand extends Model {
     /** @use HasFactory<\Database\Factories\BrandFactory> */
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'is_active',
+    ];
 }
