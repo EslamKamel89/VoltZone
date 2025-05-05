e8abf42466313d019260cd3ce9e3f74771aa731d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-05 21:49:53 +0300

    feat: Replace anchor tags with Flux modal triggers for category and brand components

--- resources/views/livewire/categories/comps/category.blade.php
+++ resources/views/livewire/categories/comps/category.blade.php
@@ -7,14 +7,33 @@ new class extends Component {
     public Category $category;
 }; ?>
 
-<a href="#" class="block overflow-hidden transition-shadow bg-white border border-gray-100 shadow-sm group rounded-xl hover:shadow-md">
-    <div class="aspect-w-1 aspect-h-1">
-        <img
-            src="{{ asset('storage/'.$category->image) }}"
-            alt="{{ $category->name }}"
-            class="object-cover w-full h-48 transition-transform duration-300 group-hover:scale-105">
-    </div>
-    <div class="p-4 text-center">
-        <h3 class="text-lg font-medium text-gray-900">{{ $category->name }}</h3>
-    </div>
-</a>
+<div>
+
+    <flux:modal.trigger name="{{ 'category'.$category->id }}">
+        <div class="block overflow-hidden transition-shadow bg-white border border-gray-100 shadow-sm group rounded-xl hover:shadow-md">
+            <div class="aspect-w-1 aspect-h-1">
+                <img
+                    src="{{ asset('storage/'.$category->image) }}"
+                    alt="{{ $category->name }}"
+                    class="object-cover w-full h-48 transition-transform duration-300 group-hover:scale-105">
+            </div>
+            <div class="p-4 text-center">
+                <h3 class="text-lg font-medium text-gray-900">{{ $category->name }}</h3>
+            </div>
+        </div>
+    </flux:modal.trigger>
+
+    <flux:modal name="{{ 'category'.$category->id }}" class=" md:w-96">
+        <div class="block overflow-hidden transition-shadow bg-white group rounded-xl hover:shadow-md">
+            <div class="aspect-w-1 aspect-h-1">
+                <img
+                    src="{{ asset('storage/'.$category->image) }}"
+                    alt="{{ $category->name }}"
+                    class="object-cover w-full h-48 transition-transform duration-300 group-hover:scale-105">
+            </div>
+            <div class="p-4 text-center">
+                <h3 class="text-lg font-medium text-gray-900">{{ $category->name }}</h3>
+            </div>
+        </div>
+    </flux:modal>
+</div>
--- resources/views/livewire/home/comps/brand.blade.php
+++ resources/views/livewire/home/comps/brand.blade.php
@@ -7,9 +7,24 @@ new class extends Component {
     public Brand $brand;
 }; ?>
 
-<div class="flex flex-col items-center justify-center p-6 transition-all bg-white border border-gray-100 shadow-sm rounded-xl hover:shadow-md group">
-    <div class="flex items-center justify-center w-16 h-16 mb-3">
-        <img src="{{ asset('storage/'.$brand->image) }}" alt="{{ $brand->name }}" class="object-contain h-12">
-    </div>
-    <h3 class="font-medium text-gray-900">{{ $brand->name }}</h3>
+
+
+<div>
+    <flux:modal.trigger name="{{ 'brand'.$brand->id }}">
+        <div class="flex flex-col items-center justify-center p-6 transition-all bg-white border border-gray-100 shadow-sm rounded-xl hover:shadow-md group">
+            <div class="flex items-center justify-center w-16 h-16 mb-3">
+                <img src="{{ asset('storage/'.$brand->image) }}" alt="{{ $brand->name }}" class="object-contain h-12">
+            </div>
+            <h3 class="font-medium text-gray-900">{{ $brand->name }}</h3>
+        </div>
+    </flux:modal.trigger>
+
+    <flux:modal name="{{ 'brand'.$brand->id }}" class="md:w-96">
+        <div class="flex flex-col items-center justify-center p-6 transition-all bg-white rounded-xl hover:shadow-md group">
+            <div class="flex items-center justify-center w-16 h-16 mb-3">
+                <img src="{{ asset('storage/'.$brand->image) }}" alt="{{ $brand->name }}" class="object-contain h-12">
+            </div>
+            <h3 class="font-medium text-gray-900">{{ $brand->name }}</h3>
+        </div>
+    </flux:modal>
 </div>
