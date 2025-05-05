bdbca7e0dc43526f8a17e6472dc0250a2d333c56
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-05 21:37:03 +0300

    feat: Implement category component and update categories index to use it

---
+++ resources/views/livewire/categories/comps/category.blade.php
@@ -0,0 +1,20 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\Category;
+
+new class extends Component {
+    public Category $category;
+}; ?>
+
+<a href="#" class="block overflow-hidden transition-shadow bg-white border border-gray-100 shadow-sm group rounded-xl hover:shadow-md">
+    <div class="aspect-w-1 aspect-h-1">
+        <img
+            src="{{ asset('storage/'.$category->image) }}"
+            alt="{{ $category->name }}"
+            class="object-cover w-full h-48 transition-transform duration-300 group-hover:scale-105">
+    </div>
+    <div class="p-4 text-center">
+        <h3 class="text-lg font-medium text-gray-900">{{ $category->name }}</h3>
+    </div>
+</a>
