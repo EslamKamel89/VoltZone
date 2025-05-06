2b1441e680a438e2573770092d803395954ac209
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-06 19:49:53 +0300

    feat: Add Spatie query builder package and update layout styles for improved responsiveness

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -5,7 +5,7 @@
     @include('partials.head')
 </head>
 
-<body class="flex flex-col min-h-screen bg-white dark:bg-zinc-800">
+<body class="flex flex-col w-screen min-h-screen bg-white dark:bg-zinc-800">
     <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
@@ -106,7 +106,7 @@
 </flux:navlist>
 -->
     </flux:sidebar>
-    <div class="flex-1">
+    <div class="flex-1 w-screen overflow-x-hidden">
 
         {{ $slot }}
     </div>
