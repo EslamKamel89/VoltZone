23c88d47ae704617fe6b77d28f293066cc5e4fb3
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-11 21:37:50 +0300

    feat: Implement CartManagment service with initial todo methods for cart operations

---
+++ app/Services/CartManagment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class CartManagment {
+    // todo: add item to cart
+
+    // todo: remove item from cart
+
+    // todo: add cart items to cookie
+
+    // todo: clear cart tiems from cookie
+
+    // todo: get all cart items from cookie
+
+    // todo: increment item quantity
+
+    // todo: decrement item quantity
+
+    // todo: calculate grand total
+}
