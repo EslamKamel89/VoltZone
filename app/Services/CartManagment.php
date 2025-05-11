9674be52bb7d591a0a66850c7af6c78e5e5208de
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-11 21:46:30 +0300

    feat: Define cookie name and expiry time in CartManagment service

--- app/Services/CartManagment.php
+++ app/Services/CartManagment.php
@@ -3,11 +3,20 @@
 namespace App\Services;
 
 class CartManagment {
+    static public string $cookieName = '$cart_items';
+    static public int $expiryTime = 60 * 24 * 30;
     // todo: add item to cart
 
     // todo: remove item from cart
 
-    // todo: add cart items to cookie
+    // add cart items to cookie
+    static public function addcartItemsToCookie(array $cart_items) {
+        cookie(
+            self::$cookieName,
+            json_encode($cart_items),
+            self::$expiryTime
+        );
+    }
 
     // todo: clear cart tiems from cookie
 
